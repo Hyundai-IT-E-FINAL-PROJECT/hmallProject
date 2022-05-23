5dee75dc45a80994797bb0e7413c4f312b2387b8
unknown <alstntn2631@naver.com>
2022-05-23 10:33:32 +0900

    [FEAT]: mypage 권한기능 추가

--- src/main/java/org/team2/security/CustomLoginSuccessHandler.java
+++ src/main/java/org/team2/security/CustomLoginSuccessHandler.java
@@ -36,10 +36,16 @@ public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
             return;
         }
 
-        if(roleNames.contains("ROLE_MEMBER")){
-            response.sendRedirect("/user/member");
+//        if(roleNames.contains("ROLE_MEMBER")){
+//            response.sendRedirect("/user/member");
+//            return;
+//        }
+
+        if(roleNames.contains("ROLE_USER")){
+            response.sendRedirect("/");
             return;
         }
+
 
         response.sendRedirect("/");
     }
