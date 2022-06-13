41abcd0355ed911e5b4320a5a46ad32cd88a8657
asd3638 <asd36384@gmail.com>
2022-06-13 21:38:16 +0900

    add cross origin annotation

--- src/main/java/org/team2/controller/product/CategoryController.java
+++ src/main/java/org/team2/controller/product/CategoryController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class CategoryController {
     @Setter(onMethod_ = @Autowired)
     private CategoryService categoryService;
 
+    @CrossOrigin("*")
     @GetMapping(value = "/")
     public ResponseEntity<List<CategoryVO>> getAll(){
         List<CategoryVO> categoryVOS = categoryService.getAll();
